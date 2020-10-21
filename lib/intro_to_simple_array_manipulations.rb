476ed327144113190d036a66c9130becd08ca75d
Blaire Baker <bakerblaire@gmail.com>
2020-10-21 18:52:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,8 +11,7 @@ def using_pop (array)
 end
 
 def pop_with_args (array)
-  array.pop
-  array.pop
+  array.pop(2)
 end
 
 def using_shift (array)
@@ -20,6 +19,5 @@ def using_shift (array)
 end
 
 def shift_with_args (array)
-  array.shift
-  array.shift
+  array.shift(2)
 end
